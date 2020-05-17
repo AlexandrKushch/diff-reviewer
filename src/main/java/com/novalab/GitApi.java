31c734584697b24de23511c62922ae52007cda52
28b-kushch <a.kushch0@gmail.com>
2020-05-17 11:05:05 +0300

    Added comments to GitApi

--- src/main/java/com/novalab/GitApi.java
+++ src/main/java/com/novalab/GitApi.java
@@ -6,23 +6,28 @@ import io.gitea.Configuration;
 import io.gitea.api.IssueApi;
 import io.gitea.api.RepositoryApi;
 import io.gitea.auth.ApiKeyAuth;
-import io.gitea.model.AccessToken;
 import io.gitea.model.Comment;
 import io.gitea.model.PullRequest;
-import io.gitea.model.User;
-import org.threeten.bp.OffsetDateTime;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class GitApi {
+    /// Input url which enter user.
     private final String url = "https://try.gitea.io/AlexKushch/test/pulls/2";  //https://gitea.novalab.live/novalab-pool/diff-reviewer/pulls/3
 
+    /// Pull request title.
     private String titlePR;
+    /// Pull request description.
     private String descriptionPR;
+    /// Pull request creator.
     private String creatorPR;
+    /// Pull request status.
     private Status statusPR;
+    /// URL to diff file.
     private String diffURL;
+    /// List of comments in pull request discussions.
+    private List<Comment> discussions = new ArrayList<Comment>();
 
     private void setTitlePR(String titlePR) {
         this.titlePR = titlePR;
@@ -45,18 +50,21 @@ public class GitApi {
     }
 
     public void GetPR() {
+        /// Set configuration and access token.
         ApiClient defaultClient = Configuration.getDefaultApiClient();
         defaultClient.setBasePath("https://try.gitea.io/api/v1");
 
         ApiKeyAuth accessToken = (ApiKeyAuth) defaultClient.getAuthentication("AccessToken");
         accessToken.setApiKey("1c507ac020604c7a30315569fd6ccde908a64a25");  // 4399ec28ea4be7de3d5bfbc22c0e8c9da1058c62
 
+        /// Divide url by '/' and get owner, repo and index from url.
         String [] split = url.split("/");
 
-        String owner = split[3];
-        String repo = split[4];
-        Long index = Long.parseLong(split[split.length-1]);
+        String owner = split[3]; // Owner in url always the third.
+        String repo = split[4]; // Repo goes after owner
+        Long index = Long.parseLong(split[split.length-1]); // Index always the last in url.
 
+        /// Get PR info.
         RepositoryApi repositoryApi = new RepositoryApi();
         PullRequest result = new PullRequest();
         try {
@@ -65,13 +73,14 @@ public class GitApi {
             e.printStackTrace();
         }
 
-        setTitlePR(result.getTitle());
-        setDescriptionPR(result.getBody());
-        setCreatorPR(result.getUser().getLogin());
+        setTitlePR(result.getTitle());  // Get title.
+        setDescriptionPR(result.getBody()); // Get description.
+        setCreatorPR(result.getUser().getLogin());  // Get creator.
 
+        /// Class 'PullRequest' has two field. State(open, close), Merged(merged, not merged).
         String state = result.getState();
-        if(state.equals("open")){
-            if(result.getMergedBy() != null){
+        if(state.equals("open")){       // First of all check by State. If is 'open' then
+            if(result.getMergedBy() != null){   // Check by merged.
                 setStatusPR(Status.MERGED);
             } else {
                 setStatusPR(Status.NOT_MERGED);
@@ -80,13 +89,14 @@ public class GitApi {
             setStatusPR(Status.CLOSED);
         }
 
+        /// All comments are located in list of all comments. Here issues comments and PR comments.
+        /// But has a different, comments from PR have a link, issue hasn't.
         IssueApi issueApi = new IssueApi();
-        List<Comment> discussions = new ArrayList<Comment>();
         try {
             discussions = issueApi.issueGetRepoComments(owner, repo, "");
             for(int i = 0; i < discussions.size(); i++){
                 if(!discussions.get(i).getPullRequestUrl().equals(url)){
-                    discussions.remove(i);
+                    discussions.remove(i);      // Remove from discussions comments which located in issues
                     i--;
                 }
             }
@@ -96,6 +106,6 @@ public class GitApi {
 
         setDiffURL(result.getDiffUrl());
 
-        System.out.println(result);
+        System.out.println(discussions);
     }
 }
